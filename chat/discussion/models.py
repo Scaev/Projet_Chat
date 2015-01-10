0c2653759e75b52fa65379af868c1aec6ef01eeb
robin69300 <robin.monnier@eleves.enpc.fr>
2015-01-11 00:51:50 +0100

    implementation liste discussion, 1 erreur restante

--- chat/discussion/models.py
+++ chat/discussion/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 class Utilisateur(models.Model):
     pseudo = models.CharField(max_length=20)
-    mot_de_passe = models.CharField(max_length=20)    
+    mot_de_passe = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.pseudo
+
+  
     
     
 class Message(models.Model):
     texte = models.CharField(max_length=500)
     date = date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date d'envoi")
-    auteur = models.ForeignKey('Utilisateur')
+    auteur = models.ForeignKey('Utilisateur', related_name="messages")
 
 class Conversation(models.Model):
-    titre = models.CharField(max_length=500)
-    participants = models.ManyToManyField(Utilisateur)
+    participants = models.ManyToManyField(Utilisateur,related_name="conversations")
     messages = models.ManyToManyField(Message)
-    
+
