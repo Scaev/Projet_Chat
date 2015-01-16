3ebdf249f73be0e9d18075f33f5531d626246ddf
robin69300 <robin.monnier@eleves.enpc.fr>
2015-01-16 22:50:46 +0100

    fix ajout conversation

--- chat/discussion/urls.py
+++ chat/discussion/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('discussion.views',
                            url(r'^connexion$', 'connexion'),
                            url(r'^inscription$', 'inscription'),
                            url(r'^accueil$', 'accueil'),
-                           #url(r'^creation_conversation/(?P<pseudo_utilisateur>\w+)$','creation_conversation'),
+                           url(r'^conversations/(?P<pseudo_utilisateur>\w+)/creation_conversation$','creation_conversation'),
+                           url(r'^discussion/(?P<id_discussion>\w+)$', 'discussion'),
                        )
