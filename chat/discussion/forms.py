e15bb43212fab2d574e62971f067ec598cb56849
CharlesReizine <creizine@gmail.com>
2015-01-29 15:09:18 +0100

    Premiers tests redigés, modification esthétique du formulaire de connexion ( ajout css) et des boutons

--- chat/discussion/forms.py
+++ chat/discussion/forms.py
@@ -11,7 +11,7 @@ class InscriptionForm(forms.Form):
     
     pseudo=forms.CharField(min_length=1,max_length=20,required=False)
     telephone=forms.CharField(required=False)
-    email=forms.EmailField(label="Votre adresse mail",required=False)
+    email=forms.EmailField(label="Mail",required=False)
     mdp = forms.CharField(widget=forms.PasswordInput,min_length=1,label="mot de passe",required=False)
     
 
@@ -73,7 +73,7 @@ class InscriptionForm(forms.Form):
 
 
 class ConnexionForm(forms.Form):
-    identifiant=forms.CharField(max_length=20,label="inserez votre adresse mail, pseudo ou telephone svp \n",required=False)
+    identifiant=forms.CharField(max_length=20,label="Pseudo, mail ou telephone",required=False)
     mdp = forms.CharField(widget=forms.PasswordInput,required=False)
     
     def clean_identifiant(self):
@@ -122,7 +122,7 @@ class ChangementForm(forms.Form):
     pseudo=forms.CharField(min_length=1,max_length=20,required=False)
     telephone=forms.CharField(required=False)
     email=forms.EmailField(label="Votre adresse mail",required=False)
-    mdp = forms.CharField(widget=forms.PasswordInput,min_length=1,label="nouveau mot de passe",required=False)
+    mdp = forms.CharField(widget=forms.PasswordInput,min_length=1,label="Nouveau mot de passe",required=False)
     
     def clean_telephone(self):
         telephone = self.cleaned_data['telephone']
